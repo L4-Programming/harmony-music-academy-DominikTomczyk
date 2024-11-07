748711cfeb3dab531f0c617436ff6c836aed791e
DominikTomczyk <0641614@hc.ac.je>
2024-11-07 12:15:30 +0000

    Added input ids and event listener

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // javascript
 
-// Display a form to the user - Done
-// Include input fields for the user's email address, level, and hours of study - Done
-
 // Capture user's input on form submission - Set to variables
 //  Get the user's email address - userEmail
 //  Get the user's level - userLevel
 //  Get the user's hours of study - userHours
-let formData = document.getElementById("InputForm");
+let formData = document.querySelector("form");
+
+formData.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("Form submitted");
+});
 
 let userEmail;
 let userLevel;
 let userHours;
+
+console.log(userEmail);
+console.log(userLevel);
+console.log(userHours);
 
 // Validate the user's input - check with ifs. display errors by showing hidden text
 //  Check if the user has provided an email address - "E-mail address is required" is it an actual email
@@ -31,11 +37,11 @@ function ValidateData() {
     console.log("hour too low");
   }
 
-  if (userLevel == "Beginner" && userHours > 5) {
+  if (userLevel == "basic" && userHours > 5) {
     console.log("beginner hours to high");
   }
 
-  if (userLevel == "Advanced" && userHours > 10) {
+  if (userLevel == "advanced" && userHours > 10) {
     console.log("advanced hours to high");
   }
 }
