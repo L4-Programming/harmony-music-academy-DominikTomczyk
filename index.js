54b778b4847788e2a1c807f8f8934aacadeca38d
DominikTomczyk <0641614@hc.ac.je>
2024-11-07 11:09:32 +0000

    Added comment plan and beginnings of logic

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 // javascript
 
-console.log("Hello from index.js!");
+// Display a form to the user - Done
+// Include input fields for the user's email address, level, and hours of study - Done
+
+// Capture user's input on form submission - Set to variables
+//  Get the user's email address - userEmail
+//  Get the user's level - userLevel
+//  Get the user's hours of study - userHours
+let formData = document.getElementById("InputForm");
+
+let userEmail;
+let userLevel;
+let userHours;
+
+// Validate the user's input - check with ifs. display errors by showing hidden text
+//  Check if the user has provided an email address - "E-mail address is required" is it an actual email
+//  Check if the user has selected a level - "You must select a course level"
+//  Check if the user has specified at least one hour of study - "You must select at least one hour per week of lessons"
+//  Check if the number of hours requested is within the allowed range - "You can only have a maximum of 5 hours per week with the beginner plan" / "You can only have a maximum of 10 hours per week with the advanced plan"
+function ValidateData() {
+  if (userEmail == "") {
+    console.log("no email provided");
+  }
+
+  if (userLevel == "") {
+    console.log("no level provided");
+  }
+
+  if (userHours < 1) {
+    console.log("hour too low");
+  }
+
+  if (userLevel == "Beginner" && userHours > 5) {
+    console.log("beginner hours to high");
+  }
+
+  if (userLevel == "Advanced" && userHours > 10) {
+    console.log("advanced hours to high");
+  }
+}
+
+// Calculate the total cost - MATHS
+// Display the total cost to the user - add hidden text and show it with the value after calc
